f971d4d60b872f9e3115c9a32dd22e30a94dfb4c
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-04-14 09:01:47 -0400

    Finished the very basic inches to meter conversion app.

--- app/src/main/java/com/example/inchesconverter/MainActivity.java
+++ app/src/main/java/com/example/inchesconverter/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.inchesconverter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText inchesEditText;
+    Button calculateButton;
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,36 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        findViews();
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inchesString = inchesEditText.getText().toString();
+                if (inchesString.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please input a value", Toast.LENGTH_SHORT);
+                } else {
+                    double result = calculateHeight(inchesString);
+                    displayResult(result);
+                }
+            }
+        });
     }
+
+    private void findViews() {
+        inchesEditText = findViewById(R.id.edit_text_inches);
+        calculateButton = findViewById(R.id.button_calculate);
+        resultTextView = findViewById(R.id.text_view_result);
+    }
+
+    private double calculateHeight(String inchesString) {
+        int inches = Integer.parseInt(inchesString);
+        return inches * 0.0254;
+    }
+
+    private void displayResult(double result) {
+        DecimalFormat myFormatter = new DecimalFormat("0.00");
+        String resultString = myFormatter.format(result);
+        resultTextView.setText(resultString + " meters");
+    }
+
 }
